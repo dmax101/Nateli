79f2c0dbb916838b3cd6bd2c12e13f1e2bdc9316
Danilo Ribeiro <danriba@gmail.com>
2020-07-26 22:08:00 -0300

    First commit

--- source/main.py
+++ source/main.py
@@ -9,20 +9,39 @@ rec = sr.Recognizer()
 
 reproduction = pyttsx3.init()
 
-def sai_som(response):
-    reproduction.say(response)
+def sai_som(resp):
+    reproduction.say(resp)
     reproduction.runAndWait()
 
+def response(self, entities, intents, text, traits):
+    if (intents['name'] == 'action') and (intents['confidence'] > 0.6):
+        if not entities:
+            sai_som("não encontrei nada")
+        else:
+            if entities['value'] == 'on':
+                sai_som('ligado')
+            else:
+                sai_som('desligado ligado')
+    
+def response_checker(self, response):
+    if not response:
+        return False
+
 while True:
+    print("inicializando...")
     with sr.Microphone() as s:
         rec.adjust_for_ambient_noise(s)
         while True:
             try:
+                
+                print("ouvindo...")
                 audio = rec.listen(s)
-                audio_txt = rec.recognize_google(audio, language="pt")
-                print("Você disse: {}".format(audio_txt))
+                entrada = rec.recognize_google(audio, language="pt")
+                print("Você disse: {}".format(entrada))
 
-                resp = client.message(audio_txt)
+                resp = client.message(entrada)
+
+                pprint(resp)
 
                 resp_value = resp['traits']['wit$on_off'][0]['value']
 
@@ -35,7 +54,11 @@ while True:
                     sai_som("Repita")
 
             except sr.UnknownValueError:
-                print("Erro")
+                print("Não Entendi")
+            
+            except KeyError:
+                print("Erro de chave")
+                sai_som("Erro de chave")
 
 
 #client.interactive()
